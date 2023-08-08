c634b79f6f97b02f7fd5fe489015cb049ca6a4a5
prakash6855 <prakashkumar68553495@gmail.com>
2023-08-08 23:13:36 +0530

    added new file

---
+++ 1_react/src/App.js
@@ -0,0 +1,22 @@
+let name = "Raja";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <b>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ex
+          eligendi placeat neque, eveniet quia necessitatibus delectus quaerat
+          ea iste molestias illum.
+        </b>
+      </div>
+    </>
+  );
+}
+
+export default App;
