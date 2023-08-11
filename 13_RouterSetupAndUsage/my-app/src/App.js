fbdc56cefd77bec3f6cb27e5c42a17f662876155
prakash6855 <prakashkumar68553495@gmail.com>
2023-08-11 18:02:50 +0530

    added react code

--- 13_RouterSetupAndUsage/my-app/src/App.js
+++ 13_RouterSetupAndUsage/my-app/src/App.js
@@ -2,9 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
+
 
 function App() {
   const [Mode, setMode] = useState("light"); //To check whether darkmode is enabled or not
@@ -42,16 +43,13 @@ function App() {
 
   return (
     <>
-      {/* <Navbar title = "TextUtils" aboutText = "About Our Community"/> */}
+      <Navbar title = "TextUtils" aboutText = "About Our Community"/>
       {/* <Navbar/> */}
       <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-5">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter your text here..."
-          mode={Mode}
-        />
+        {/* <TextForm showAlert={showAlert} heading="Enter your text here..." mode ={Mode}/> */}
+        <About />
       </div>
     </>
   );
