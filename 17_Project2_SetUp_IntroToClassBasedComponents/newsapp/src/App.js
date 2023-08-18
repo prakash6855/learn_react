1087bcc8221933fc2fd38eb0053e3ea537691cd6
prakash6855 <prakashkumar68553495@gmail.com>
2023-08-18 15:07:11 +0530

    New project started

---
+++ 17_Project2_SetUp_IntroToClassBasedComponents/newsapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c="coding";
+  render() {
+    return (
+      <div>
+        Hi i am your first class based component so, start {this.c} here!!
+      </div>
+    )
+  }
+}
